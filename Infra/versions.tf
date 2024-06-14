09d4c5064f340d909b4caa5aa0eee007ed170bc4
David Wahby <wasytb@gmail.com>
2024-06-14 09:17:04 +0200

    Added Deployed Tested with yaml for the app

--- Infra/versions.tf
+++ Infra/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.1.0"
+      version = "3.67.0" // Updated the version to the latest version. Original version is 3.1 It did not work.
     }
     azuread = {
       source  = "hashicorp/azuread"
