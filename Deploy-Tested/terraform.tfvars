af639bea53fac9c0de177df2a6f4f739d63ec8df
David Wahby <wasytb@gmail.com>
2024-06-14 09:20:12 +0200

    Added Deployed Tested with yaml for the app

---
+++ Deploy-Tested/terraform.tfvars
@@ -0,0 +1,4 @@
+resource_group_name = "rg-aks-identity"
+location = "eastus"
+aks_name = "aks-identity"
+acr_name = "aks-identity-acr"
---
+++ Deploy-Tested/vars.tf
@@ -0,0 +1,16 @@
+variable "resource_group_name" {
+  description = "The name of the resource group in which to create the demo registry."
+  type = string
+}
+variable "location" {
+  description = "The location/region where the demo registry should be created."
+  type = string 
+}
+variable "aks_name" {
+  description = "The name of the AKS cluster."
+  type = string
+}
+variable "acr_name" {
+  description = "The name of the Azure Container Registry."
+  type = string 
+}
